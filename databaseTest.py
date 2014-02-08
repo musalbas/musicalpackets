b06dea60494304065dddd3dd32248b84e08c91fe
= <spoffeh@gmail.com>
2014-02-08 21:06:10 +0000

    Updated the test file.

--- databaseTest.py
+++ databaseTest.py
@@ -4,17 +4,20 @@ from getdata import getdata
 import time
 
 database = musicalDatabase.request_database("MusicalPackets")
-database._database.drop_collection("MyFirstMusic")
-collection = database.open_collection("MyFirstMusic")
+#database._database.drop_collection("MyFirstMusic")
+#collection = database.open_collection("MyFirstMusic")
 
 
-data = getdata()
-global counter
-counter = 0
-for packet in data:
-    counter = counter + 1
-    packet['time'] = time.time() + counter
-    collection.put(packet)
+#data = getdata()
+#global counter
+#counter = 0
+#for packet in data:
+#    counter = counter + 1
+#    packet['time'] = time.time() + counter
+#    collection.put(packet)
 
-result = stats.count_field_per_duration("MyFirstMusic", 'source', 5)
+
+result = stats.count_field_per_duration("packets", 'source_ip', 50)
+for data in result:
+    print(data)
 
