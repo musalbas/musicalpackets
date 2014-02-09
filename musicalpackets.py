ab56674c05db0e695acd54467d65c7941070f326
= <spoffeh@gmail.com>
2014-02-09 10:53:39 +0000

    Made it sounds like music!

--- musicalpackets.py
+++ musicalpackets.py
@@ -3,6 +3,11 @@ import thread
 import sys
 
 from mingus.midi import fluidsynth
+from mingus.containers.NoteContainer import NoteContainer
+from mingus.containers.Note import Note
+import mingus.core.scales as scales
+import mingus.core.chords as chords
+
 
 import musicalDatabase
 import server
@@ -103,6 +108,7 @@ class PacketAnalyser:
 class MusicalPackets:
 
     _step_interval = 0.5
+    _current_num = 0
 
     def __init__(self):
         self._packetlistener = PacketListener()
@@ -113,13 +119,39 @@ class MusicalPackets:
         self._music_loop()
 
     def _music_loop(self):
-        current_note = 0
+        current_num = 0
 
         while True:
-            fluidsynth.stop_Note(current_note, 0)
-            current_note = self._packetanalyser.get_average_pps() % 85
-            fluidsynth.play_Note(current_note, 0, 100)
+            print(self._packetanalyser.get_average_pps())
+            self._current_num = self._packetanalyser.get_average_pps() % 85
+            self.play_note(discord=0)
             time.sleep(self._step_interval)
+            self.stop_note()
+    
+    def play_note(self, discord=0):
+       if discord:
+            self._current_note = self.generate_discord_container(self._current_num)
+       elif self._current_num == 0:
+            self._current_note = self.generate_base_container("C")
+       else:
+            self._current_note = self.generate_triad_container(self._current_num, "C") 
+       fluidsynth.play_NoteContainer(self._current_note, 0, 100)
+
+    def stop_note(self):
+        fluidsynth.stop_NoteContainer(self._current_note, 0)
+        
+    def generate_triad_container(self, number, key):
+        scale = scales.diatonic(key)
+        pos = number % len(scale)
+        triad = chords.triad(scale[pos], key)
+        return NoteContainer(triad) 
+    
+    def generate_discord_container(self, number):
+        return NoteContainer(Note(number))
+
+    def generate_base_container(self, key):
+        return NoteContainer(Note(scales.diatonic(key)[0], 2))
+
 
 if __name__ == "__main__":
     thread.start_new_thread(server.start, ())
